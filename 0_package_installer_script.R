0970ef83472414ea76665451a48dbc0a24885b20
Remian <blksakib@gmail.com>
2025-04-11 16:53:31 -0600

    Add files via upload

---
+++ 0_package_installer_script.R
@@ -0,0 +1,13 @@
+
+if (!require("caret")) install.packages("caret")
+if (!require("effects")) install.packages("effects")
+if (!require("readr")) install.packages("readr")
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("tidyr")) install.packages("tidyr")
+if (!require("WriteXLS")) install.packages("WriteXLS")
+if (!require("ggplot2")) install.packages("ggplot2")
+if (!require("reshape2")) install.packages("reshape2")
+if (!require("car")) install.packages("car")
+if (!require("glmnet")) install.packages("glmnet")
+
+
